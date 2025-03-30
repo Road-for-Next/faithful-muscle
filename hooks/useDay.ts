d91e02e23c72611edc527498329645ecda2d96e2
millennium00 <71242138+Woolegend@users.noreply.github.com>
2025-03-31 08:05:14 +0900

    useDay 커스텀 훅 개선 (#18)
    * fix: setDay 파라미터 타입 string에서 Day로 수정
    
    * docs: useDay 주석 추가
    
    * feat: number 타입 요일 변경 시 예외처리

--- hooks/useDay.ts
+++ hooks/useDay.ts
@@ -6,12 +6,18 @@ const DAY_LIST: Day[] = ['sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat'];
 
 const initializeDay = () => DAY_LIST[new Date().getDay()];
 
+/**
+ * 요일 관리 커스텀 훅
+ * @property day - 선택된 요일 / 기본값 오늘의 요일
+ * @property setDay - 요일 변경
+ */
 const useDay = () => {
   const [day, set] = useState<Day>(() => initializeDay());
 
-  const setDay = (param?: number | string) => {
+  const setDay = (param?: number | Day) => {
     switch (typeof param) {
       case 'number':
+        if (param < 0 || param > 6) return;
         set(DAY_LIST[param]);
         break;
       case 'string':
