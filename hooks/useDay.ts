c96825f6f5afa2add30328baffe6406f31192cec
JaeHyeon, Woo <dnwogus4260@naver.com>
2025-03-31 07:49:24 +0900

    docs: useDay 주석 추가

--- hooks/useDay.ts
+++ hooks/useDay.ts
@@ -6,6 +6,11 @@ const DAY_LIST: Day[] = ['sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat'];
 
 const initializeDay = () => DAY_LIST[new Date().getDay()];
 
+/**
+ * 요일 관리 커스텀 훅
+ * @property day - 선택된 요일 / 기본값 오늘의 요일
+ * @property setDay - 요일 변경
+ */
 const useDay = () => {
   const [day, set] = useState<Day>(() => initializeDay());
 
