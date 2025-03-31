d67dd15f0b91b5f36c6778790a2c2f130ebf965c
JaeHyeon, Woo <dnwogus4260@naver.com>
2025-03-31 14:13:44 +0900

    fix: 기수법 변환 함수 BASE64에서 BASE62로 변경

--- lib/convertNumeralSystem.ts
+++ lib/convertNumeralSystem.ts
@@ -1,25 +1,35 @@
-function convert10to64(decimal: number) {
-  const base64Chars =
-    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/';
+const BASE_LENGTH = 62;
+
+const convert10to62 = (decimal: number) => {
+  const baseChars = process.env.BASE62_CHARS;
+  if (!baseChars) throw new Error('BASE62_CHARS is not defined');
+  const length = baseChars.length;
+  if (length !== BASE_LENGTH)
+    throw new Error('BASE62_CHARS length must be ' + BASE_LENGTH);
+
   let result = '';
   if (decimal === 0) {
-    return 'A';
+    return baseChars[0];
   }
   while (decimal > 0) {
-    result = base64Chars[decimal % 64] + result;
-    decimal = Math.floor(decimal / 64);
+    result = baseChars[decimal % length] + result;
+    decimal = Math.floor(decimal / length);
   }
   return result;
-}
+};
 
-function convert64to10(base64: string) {
-  const base64Chars =
-    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/';
+const convert62to10 = (base62: string) => {
+  const baseChars = process.env.BASE62_CHARS;
+  if (!baseChars) throw new Error('BASE62_CHARS is not defined');
+  const length = baseChars.length;
+  if (length !== BASE_LENGTH)
+    throw new Error('BASE62_CHARS length must be ' + BASE_LENGTH);
+
   let result = 0;
-  for (let i = 0; i < base64.length; i++) {
-    result = result * 64 + base64Chars.indexOf(base64[i]);
+  for (let i = 0; i < base62.length; i++) {
+    result = result * length + baseChars.indexOf(base62[i]);
   }
   return result;
-}
+};
 
-export { convert10to64, convert64to10 };
+export { convert10to62, convert62to10 };
