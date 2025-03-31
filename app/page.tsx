c8c1d674184a6a4ba242d4b721e3ef1e84ccc44d
JaeHyeon, Woo <dnwogus4260@naver.com>
2025-03-31 14:57:32 +0900

    feat: 홈페이지에 테마 선택자 배치

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,12 @@
+import { ThemeSelector } from '@/components/ThemeSelector';
+
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      <h1>Home</h1>
+      <div className="fixed top-2 right-2 z-10">
+        <ThemeSelector />
+      </div>
+    </div>
+  );
 }
