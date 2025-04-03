743ab13971b2e138d48253c26815e05c5b4a7f0e
millennium00 <71242138+Woolegend@users.noreply.github.com>
2025-04-03 10:51:29 +0900

    공용 Container 및 테마 선택 컴포넌트 구현 (#24)
    * chore: next-theme 설치
    
    * chore: shadcn Button 컴포넌트 설치
    
    * chore: shadcn Dropdown menu 컴포넌트 설치
    
    * feat: ThemeProvider로 최상위 layout children 랩핑
    
    * feat: 테마 선택 컴포넌트 구현
    
    * feat: 홈페이지에 테마 선택자 배치
    
    * feat: Container 컴포넌트 구현
    
    * feat: 홈페이지에 Container 랩핑

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,14 @@
+import { Container } from '@/components/Container';
+import { ThemeSelector } from '@/components/ThemeSelector';
+
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      <Container className="relative">
+        <div className="flex items-center justify-between">
+          <h1>Home</h1> <ThemeSelector />
+        </div>
+      </Container>
+    </div>
+  );
 }
