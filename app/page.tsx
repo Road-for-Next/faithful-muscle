0b137414f44fa13ca634915f6ce02ed488dada9e
JaeHyeon, Woo <dnwogus4260@naver.com>
2025-04-07 22:13:54 +0900

    feat: ExerciseCard 컴포넌트 배치

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,21 @@
 import { Container } from '@/components/Container';
 import { ThemeSelector } from '@/components/ThemeSelector';
+import ExerciseCard from '@/components/ExerciseCard';
+import { COLUMN_DATA } from '@/mock/column';
 
 export default function Home() {
+  const columnData = COLUMN_DATA;
+
   return (
-    <div>
-      <Container className="relative">
-        <div className="flex items-center justify-between">
-          <h1>Home</h1> <ThemeSelector />
-        </div>
-      </Container>
-    </div>
+    <Container>
+      <div className="flex items-center justify-between">
+        <h1>Home</h1> <ThemeSelector />
+      </div>
+      <div className="flex flex-col gap-4">
+        {columnData.map((e, i) => (
+          <ExerciseCard key={i} exercise={e} />
+        ))}
+      </div>
+    </Container>
   );
 }
