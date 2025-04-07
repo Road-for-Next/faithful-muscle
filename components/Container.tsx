1f0da567765539c88c44ac54b8980ead48ad5b87
millennium00 <71242138+Woolegend@users.noreply.github.com>
2025-04-07 22:30:11 +0900

    Tailwind CSS 설치 및 커스텀 테마 설정 (#26)
    * chore: tailwind ccs 패키지 설치
    
    * feat: Container 컴포넌트의 디바이스 스타일을 tailwind의 커스텀 테마로 설정

--- components/Container.tsx
+++ components/Container.tsx
@@ -9,7 +9,10 @@ const Container = React.forwardRef<HTMLDivElement, ContainerProps>(
   ({ className, children, ...props }, ref) => {
     return (
       <div
-        className={cn('mx-auto max-w-[430px] min-w-[360px] p-4', className)}
+        className={cn(
+          'max-w-m-max min-w-m-min mx-auto min-h-svh p-4',
+          className,
+        )}
         ref={ref}
         {...props}
       >
