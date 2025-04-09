730a733ac4ae7e7da13ce05638344de57973f678
Woolegend <dnwogus4260@naver.com>
2025-04-09 22:24:39 +0900

    feat: AddExerciseDrawer 컴포넌트 생성

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import { Container } from '@/components/Container';
 import { ThemeSelector } from '@/components/ThemeSelector';
 import ExerciseCard from '@/components/ExerciseCard';
 import { COLUMN_DATA } from '@/mock/column';
+import AddExerciseDrawer from '@/components/AddExerciseDrawer';
 
 export default function Home() {
   const columnData = COLUMN_DATA;
@@ -9,7 +10,11 @@ export default function Home() {
   return (
     <Container>
       <div className="mb-4 flex items-center justify-between">
-        <h1>Home</h1> <ThemeSelector />
+        <h1>Home</h1>
+        <div className="flex items-center gap-2">
+          <ThemeSelector />
+          <AddExerciseDrawer />
+        </div>
       </div>
       <div className="flex flex-col gap-4">
         {columnData.map((e, i) => (
