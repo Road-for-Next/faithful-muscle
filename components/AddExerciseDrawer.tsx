f3fd4324a1a9c83c87a4904cf6f698f62af60a50
JaeHyeon, Woo <dnwogus4260@naver.com>
2025-04-12 00:15:09 +0900

    feat: drawer에 운동 검색창 ui 마크업

--- components/AddExerciseDrawer.tsx
+++ components/AddExerciseDrawer.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import { Button } from '@/components/ui/button';
 import {
   Drawer,
@@ -9,10 +11,39 @@ import {
   DrawerTitle,
   DrawerTrigger,
 } from '@/components/ui/drawer';
+import { useEffect, useState } from 'react';
+import {
+  Command,
+  CommandEmpty,
+  CommandInput,
+  CommandItem,
+  CommandList,
+  CommandShortcut,
+} from './ui/command';
+import { EXERCISE_DATA, ExerciseType } from '@/mock/exercise';
 
 export default function AddExerciseDrawer() {
+  const [keyword, setKeyword] = useState('');
+  const [suggestions, setSuggestions] = useState<ExerciseType[]>([]);
+  const [selected, setSelected] = useState<ExerciseType | null>(null);
+
+  useEffect(() => console.log(selected), [selected]);
+
+  useEffect(() => {
+    if (keyword.length > 0) {
+      const filteredSuggestions: ExerciseType[] = EXERCISE_DATA.filter(
+        (exercise) =>
+          exercise.en.toLowerCase().includes(keyword.toLowerCase()) ||
+          exercise.ko.includes(keyword),
+      );
+      setSuggestions(filteredSuggestions);
+    } else {
+      setSuggestions([]);
+    }
+  }, [keyword]);
+
   return (
-    <Drawer>
+    <Drawer onClose={() => setKeyword('')}>
       <DrawerTrigger asChild>
         <Button>추가하기</Button>
       </DrawerTrigger>
@@ -23,6 +54,30 @@ export default function AddExerciseDrawer() {
             운동을 검색해서 루틴에 추가해보세요
           </DrawerDescription>
         </DrawerHeader>
+
+        <Command label="운동 검색" className="mt-4" shouldFilter={false}>
+          <CommandInput
+            value={keyword}
+            onValueChange={(value) => setKeyword(value)}
+            placeholder="운동을 검색하세요..."
+            autoFocus
+          />
+          <CommandList>
+            {keyword && <CommandEmpty>No results found.</CommandEmpty>}
+            {suggestions.map((e) => (
+              <CommandItem
+                key={e.id}
+                keywords={[e.ko, e.en]}
+                onSelect={() => setSelected(e)}
+              >
+                <span>{e.ko}</span>
+                <span className="text-muted-foreground text-xs">{e.en}</span>
+                <CommandShortcut>{e.id}</CommandShortcut>
+              </CommandItem>
+            ))}
+          </CommandList>
+        </Command>
+
         <DrawerFooter>
           <DrawerClose asChild>
             <Button variant="outline">Cancel</Button>
