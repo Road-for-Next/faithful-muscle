0d33435857308478af063c14775213cc2152ac8f
Woolegend <dnwogus4260@naver.com>
2025-04-14 01:08:49 +0900

    feat: 메인 페이지에 DaySelector 배치

--- app/page.tsx
+++ app/page.tsx
@@ -6,24 +6,28 @@ import ExerciseCard from '@/components/ExerciseCard';
 import { COLUMN_DATA, ColumnType, RowType } from '@/mock/column';
 import AddExerciseDrawer from '@/components/AddExerciseDrawer';
 import { useState } from 'react';
+import DaySelector from '@/components/DaySelector';
 
 export default function Home() {
-  const [Data, setData] = useState<ColumnType>(COLUMN_DATA);
+  const [data, setData] = useState<ColumnType>(COLUMN_DATA);
+  const [day, setDay] = useState(new Date().getDay());
 
   const handleAddExercise = (exercise: RowType) =>
     setData((prev) => [...prev, exercise]);
 
+  const handleSelectDay = (day: number) => setDay(day);
+
   return (
     <Container>
       <div className="mb-4 flex items-center justify-between">
-        <h1>Home</h1>
+        <DaySelector day={day} onSelect={handleSelectDay} />
         <div className="flex items-center gap-2">
           <ThemeSelector />
           <AddExerciseDrawer onAdd={handleAddExercise} />
         </div>
       </div>
       <div className="flex flex-col gap-4">
-        {Data.map((e, i) => (
+        {data.map((e, i) => (
           <ExerciseCard key={i} exercise={e} />
         ))}
       </div>
