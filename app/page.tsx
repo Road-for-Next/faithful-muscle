7a6fd16e3aafc0ddd16bb760f6244accc3a491a6
millennium00 <71242138+Woolegend@users.noreply.github.com>
2025-04-27 19:26:07 +0900

    set 데이터 추가/삭제하기 기능 구현 (#36)
    * feat: RowCard 컴포넌트에 set 추가하기 버튼 마크업
    
    * feat: RowCard 컴포넌트에 디자인 아이콘 추가
    
    * refactor: Row 데이터 구분을 위한 id 프로퍼티 추가
    
    * chore: shadcn input, label 컴포넌트 설치
    
    * feat: set 데이터 추가하기 기능 구현
    
    * feat: set 데이터 삭제하기 기능 구현

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@
 import { Container } from '@/components/Container';
 import { ThemeSelector } from '@/components/ThemeSelector';
 import RowCard from '@/components/RowCard';
-import { COLUMN_DATA, ColumnType, RowType } from '@/mock/column';
+import { COLUMN_DATA, ColumnType, RowType, SetType } from '@/mock/column';
 import AddRowDrawer from '@/components/AddRowDrawer';
 import { useState } from 'react';
 import DaySelector from '@/components/DaySelector';
@@ -12,11 +12,38 @@ export default function Home() {
   const [data, setData] = useState<ColumnType[]>([COLUMN_DATA]);
   const [day, setDay] = useState(new Date().getDay());
 
-  const handleAddRow = (row: RowType) => {
+  const handleAddRow = (exerciseId: string) => {
     setData((prev) => {
+      const row: RowType = {
+        id: Date.now().toString() + '-' + prev[day]?.length || '0',
+        exerciseId: exerciseId,
+        sets: [],
+      };
       const next = [...prev];
       if (next[day]) next[day] = [...next[day], row];
       else next[day] = [row];
+      return next;
+    });
+  };
+
+  const handleAddRowSet = (id: string, set: SetType) => {
+    setData((prev) => {
+      const next = [...prev];
+      next[day] = next[day].map((e) =>
+        e.id === id ? { ...e, sets: [...e.sets, set] } : e,
+      );
+      return next;
+    });
+  };
+
+  const handleDeleteRowSet = (id: string, index: number) => {
+    setData((prev) => {
+      const next = [...prev];
+      next[day] = next[day].map((e) => {
+        if (e.id !== id) return e;
+        const nextSets = e.sets.filter((_, i) => i !== index);
+        return { ...e, sets: nextSets };
+      });
       return next;
     });
   };
@@ -34,7 +61,14 @@ export default function Home() {
       </div>
       <div className="flex flex-col gap-4">
         {data[day]?.length > 0 || <div>등록된 운동 계획이 없습니다.</div>}
-        {data[day]?.map((e, i) => <RowCard key={i} row={e} />)}
+        {data[day]?.map((e) => (
+          <RowCard
+            key={e.id}
+            row={e}
+            onAdd={handleAddRowSet}
+            onDelete={handleDeleteRowSet}
+          />
+        ))}
       </div>
     </Container>
   );
--- components/RowCard.tsx
+++ components/RowCard.tsx
@@ -5,39 +5,99 @@ import {
   CardAction,
   CardContent,
   CardDescription,
+  CardFooter,
   CardHeader,
   CardTitle,
 } from '@/components/ui/card';
 import { EXERCISE_DATA } from '@/mock/exercise';
-import { convert62to10 } from '@/lib/convertNumeralSystem';
-import { ChevronUp } from 'lucide-react';
-import { useEffect, useRef, useState } from 'react';
-import { RowType } from '@/mock/column';
+import { convert10to62, convert62to10 } from '@/lib/convertNumeralSystem';
+import { ChevronUp, Dumbbell, RefreshCw, X } from 'lucide-react';
+import { useCallback, useEffect, useRef, useState } from 'react';
+import { RowType, SetType } from '@/mock/column';
 import { cn } from '@/lib/utils';
+import { Button } from './ui/button';
+import { Input } from './ui/input';
+import { Label } from './ui/label';
 
 interface Props {
   row: RowType;
+  onAdd: (id: string, set: SetType) => void;
+  onDelete: (id: string, index: number) => void;
 }
 
-export default function RowCard({ row }: Props) {
+interface IValue {
+  weight: string;
+  reps: string;
+}
+
+export default function RowCard({ row, onAdd, onDelete }: Props) {
   const { exerciseId, sets } = row;
   const data = EXERCISE_DATA.find((e) => e.id === exerciseId);
-  const contentRef = useRef<HTMLDivElement>(null);
+  const bodyRef = useRef<HTMLDivElement>(null);
   const [open, setOpen] = useState(false);
   const [maxHeight, setMaxHeight] = useState(0);
+  const [isAdd, setIsAdd] = useState(false);
+  const [values, setValues] = useState<IValue>({
+    weight: '',
+    reps: '',
+  });
 
-  useEffect(() => {
-    if (contentRef.current) {
-      const contentHeight = contentRef.current.scrollHeight;
-      setMaxHeight(open ? contentHeight : 0);
+  const resetValue = () => {
+    setValues({
+      weight: '',
+      reps: '',
+    });
+  };
+
+  const toggleAdd = () => {
+    resetValue();
+    setIsAdd((prev) => !prev);
+  };
+
+  const togglebody = () => {
+    resetValue();
+    setOpen((prev) => !prev);
+    setTimeout(() => {
+      setIsAdd(false);
+    }, 150);
+  };
+
+  const handleChangeValue = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { id, value } = e.target;
+    setValues((prev) => ({ ...prev, [id]: value }));
+  };
+
+  const handleAddRowSet = () => {
+    // TODO - 유효성 검사 추가
+    if (!values.weight || !values.reps) {
+      alert('빈 칸을 입력해주세요');
+      return;
     }
+    const set: SetType = {
+      weight: convert10to62(Number(values.weight)),
+      reps: convert10to62(Number(values.reps)),
+    };
+    onAdd(row.id, set);
+    resetValue();
+  };
+
+  const handleDeleteRowSet = (index: number) => {
+    onDelete(row.id, index);
+  };
+
+  const resizeHeight = useCallback(() => {
+    if (!bodyRef.current) return;
+    const contentHeight = bodyRef.current.scrollHeight;
+    setMaxHeight(open ? contentHeight : 0);
   }, [open]);
+
+  useEffect(() => resizeHeight(), [isAdd, row.sets, resizeHeight]);
 
   return (
     <Card className="gap-0 p-3">
       <CardHeader
         className="cursor-pointer px-0 select-none"
-        onClick={() => setOpen((prev) => !prev)}
+        onClick={togglebody}
       >
         <div className="flex items-center gap-2">
           <CardTitle>{data?.ko}</CardTitle>
@@ -54,29 +114,99 @@ export default function RowCard({ row }: Props) {
           />
         </CardAction>
       </CardHeader>
-
-      <CardContent
+      <div
         className={cn(
-          'flex flex-col gap-2',
-          'mx-0 overflow-hidden px-2 py-0 text-sm',
-          'transition-all duration-300',
+          'overflow-hidden transition-all duration-300',
           'data-[open=true]:animate-fade-in',
           'data-[open=false]:animate-fade-out',
         )}
         style={{ maxHeight }}
         data-open={open}
-        ref={contentRef}
+        ref={bodyRef}
       >
-        {sets.map(({ weight, reps }, i) => (
-          <div
-            key={`${weight}-${reps}-${i}`}
-            className="flex items-center gap-4"
-          >
-            <span>중량 : {convert62to10(weight)}</span>
-            <span>반복 : {convert62to10(reps)}</span>
+        <CardContent
+          className={cn('flex flex-col gap-2', 'mx-0 px-2 py-0 text-sm')}
+        >
+          {sets.map(({ weight, reps }, i) => (
+            <div key={`${weight}-${reps}-${i}`} className="flex items-center">
+              <div className="flex grow items-center gap-4">
+                <div className="flex items-center gap-1">
+                  <Dumbbell className="size-4" />
+                  <span>중량 : {convert62to10(weight)}</span>
+                </div>
+                <div className="flex items-center gap-1">
+                  <RefreshCw className="size-4" />
+                  <span>반복 : {convert62to10(reps)}</span>
+                </div>
+              </div>
+              <Button
+                className={cn('size-6 cursor-pointer')}
+                variant="ghost"
+                onClick={() => handleDeleteRowSet(i)}
+              >
+                <X className="size-4" />
+              </Button>
+            </div>
+          ))}
+        </CardContent>
+        <CardFooter className="mt-4 flex-col gap-4 px-0">
+          {isAdd && (
+            <div
+              className={cn(
+                'animate-fade-in transition-all duration-300',
+                'flex items-center gap-2',
+                'h-9',
+              )}
+              data-open={isAdd}
+            >
+              <div className="flex items-center gap-2">
+                <Label htmlFor="weight" className="flex items-center gap-1">
+                  <Dumbbell className="size-4" />
+                  <span className="w-7">중량</span>
+                </Label>
+                <Input
+                  id="weight"
+                  name="weight"
+                  type="number"
+                  placeholder="중량"
+                  value={values.weight}
+                  onChange={handleChangeValue}
+                />
+              </div>
+              <div className="flex items-center gap-2">
+                <Label htmlFor="reps" className="flex items-center gap-1">
+                  <RefreshCw className="size-4" />
+                  <span className="block w-7">반복</span>
+                </Label>
+                <Input
+                  id="reps"
+                  name="reps"
+                  type="number"
+                  placeholder="반복"
+                  value={values.reps}
+                  onChange={handleChangeValue}
+                />
+              </div>
+            </div>
+          )}
+          <div className="flex w-full gap-2">
+            <Button
+              variant={isAdd ? 'default' : 'outline'}
+              className="grow transition-colors duration-300"
+              onClick={isAdd ? handleAddRowSet : toggleAdd}
+            >
+              {isAdd ? '완료' : '추가하기'}
+            </Button>
+            <Button
+              variant={isAdd ? 'destructive' : 'outline'}
+              className="grow transition-colors duration-300"
+              onClick={isAdd ? toggleAdd : () => {}}
+            >
+              {isAdd ? '취소' : '수정하기'}
+            </Button>
           </div>
-        ))}
-      </CardContent>
+        </CardFooter>
+      </div>
     </Card>
   );
 }
