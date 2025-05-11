d0ca548056b60e928c5d0a6054477a5b18976da8
Woolegend <dnwogus4260@naver.com>
2025-05-11 22:53:06 +0900

    feat: columns store 구현

---
+++ stores/useColumns.store.ts
@@ -0,0 +1,21 @@
+import { ColumnType } from '@/mock/column';
+import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
+
+type State = {
+  columns: ColumnType[];
+};
+
+type Actions = { setColumns: (columns: ColumnType[]) => void };
+
+const useColumnsStore = create<State & Actions>()(
+  persist(
+    (set) => ({
+      columns: [],
+      setColumns: (columns: ColumnType[]) => set({ columns }),
+    }),
+    { name: 'faithful-muscle-columns' },
+  ),
+);
+
+export default useColumnsStore;
