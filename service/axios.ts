8020fe29977f50c7a03ae109b362394f410ddc1e
JaeHyeon, Woo <dnwogus4260@naver.com>
2025-05-27 10:36:20 +0900

    fix : 경로 수정

--- service/axios.ts
+++ service/axios.ts
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
-// const BASE_URL =
-//   process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:3000';
+const BASE_URL =
+  process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:3000';
 
 const instance = axios.create({
-  baseURL: location.origin + '/api',
+  baseURL: BASE_URL + '/api',
   headers: {
     'Content-Type': 'application/json',
   },
