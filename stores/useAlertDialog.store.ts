3a56b96d3722200436f65973843936d12c456bb4
Woolegend <dnwogus4260@naver.com>
2025-06-08 13:40:16 +0900

    feat: AlertDialog Handler 처리 후 닫기 로직 적용

--- stores/useAlertDialog.store.ts
+++ stores/useAlertDialog.store.ts
@@ -8,6 +8,7 @@ type State = {
 };
 
 type Actions = {
+  // 해당 함수에서는 open=true 확정이기 때문에 Omit으로 제외함
   setOpen: (params: Omit<State, 'open'>) => void;
   setClose: () => void;
 };
@@ -15,12 +16,36 @@ type Actions = {
 // Alert Dialog의 duration 시간을 변수로 사용
 const DURATION_TIME = 200;
 
-const useAlertDialogStore = create<State & Actions>((set) => ({
+const useAlertDialogStore = create<State & Actions>((set, get) => ({
   open: false,
   title: '',
   description: '',
   handler: () => {},
-  setOpen: (params) => set({ open: true, ...params }),
+  setOpen: (params) =>
+    set(() => {
+      const { title, description, handler } = params;
+
+      // handler 처리 후 dialog를 닫는 로직을 추가
+      const wrappedHandler = async () => {
+        try {
+          // handler 처리
+          await Promise.resolve(handler());
+        } catch (error) {
+          // 에러 처리
+          alert('Error Occurred AlertDialog handler');
+          console.error('AlertDialog handler error:', error);
+        } finally {
+          // handler 처리 종료 시 dialog 닫기
+          get().setClose();
+        }
+      };
+      return {
+        open: true,
+        title,
+        description,
+        handler: wrappedHandler,
+      };
+    }),
   setClose: () => {
     set({ open: false });
     setTimeout(() => {
