fb7eb0f075b949b9fa329957ffde5d51b81cbde3
JaeHyeon, Woo <dnwogus4260@naver.com>
2025-06-09 13:38:52 +0900

    feat: RowSets 업데이트 시 toast 적용

--- components/RowCard/RowCardFooter.tsx
+++ components/RowCard/RowCardFooter.tsx
@@ -85,18 +85,37 @@ export default function RowCardFooter({
   };
 
   const handleUpdateRowSets = () => {
-    const param = valuesForEdit.map((value) => ({
-      weight: Number(value.weight),
-      reps: Number(value.reps),
-    }));
-    setOpen({
-      title: '수정하기',
-      description: '운동 계획을 수정할까요?',
-      handler: () => {
-        onUpdatetRowSets(param);
-        onChangeStatus('none');
-      },
-    });
+    const isEmpty =
+      valuesForEdit.filter((value) => !value.weight || !value.reps).length > 0;
+
+    if (isEmpty) {
+      toast.error('중량과 반복을 입력해주세요.');
+      return;
+    }
+
+    try {
+      const param = valuesForEdit.map((value) => ({
+        weight: Number(value.weight),
+        reps: Number(value.reps),
+      }));
+      setOpen({
+        title: '수정하기',
+        description: '운동 계획을 수정할까요?',
+        handler: () => {
+          try {
+            onUpdatetRowSets(param);
+            onChangeStatus('none');
+            toast.success('계획을 수정했습니다.');
+          } catch (e) {
+            toast.error('계획 수정에 실패했습니다.');
+            console.log(e);
+          }
+        },
+      });
+    } catch (e) {
+      toast.error('계획 수정에 실패했습니다.');
+      console.log(e);
+    }
   };
 
   const BUTTON_LEFT: Record<StatusType, FooterButtonProps> = {
